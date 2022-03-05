3e92c3522759b9acd71e6682e68cc8b69592039f
Sourabh <s1905singh@gmail.com>
2022-03-06 02:01:25 +0530

    written Deck class in a more systematic way

--- src/games/Deck.java
+++ src/games/Deck.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Deck {
-	ArrayList<Card> spadeCards = new ArrayList<>(10);   //creating an ArrayList of type Card of size 10
-	ArrayList<Card> faceCards = new ArrayList<>(12);    //creating an ArrayList of type Card of size 12
-	ArrayList<Card> normalCards = new ArrayList<>(30);  //creating an ArrayList of type Card of size 30
+	ArrayList<Card> spadeCards = new ArrayList<>(10);   //declaring an ArrayList of type Card of size 10
+	ArrayList<Card> faceCards = new ArrayList<>(12);    //declaring an ArrayList of type Card of size 12
+	ArrayList<Card> normalCards = new ArrayList<>(30);  //declaring an ArrayList of type Card of size 30
 	
 	public Deck(){                            //constructor
         for(int i = 1; i <=10; i++){          //all spade cards except face cards(11,12,13)
@@ -26,15 +26,6 @@ public class Deck {
         }
     }
 	
-	public ArrayList<Card> getSpadeCards() {
-		return spadeCards;
-	}
-	
-	public ArrayList<Card> getFaceCards() {
-		return faceCards;
-	}
-	
-	
 	public ArrayList<Card> concatenate(ArrayList<Card> one, ArrayList<Card> two, ArrayList<Card> three) {   //concatenate all the remaining cards
         ArrayList<Card> result = new ArrayList<Card>();
         for(Card c : one){
@@ -51,66 +42,42 @@ public class Deck {
         return result;
     }
 	
+	public void shuffleLogic(ArrayList<Card> cards) {
+		Random rand = new Random();     
+        for (int i = 0; i < cards.size() ; i++)     //shuffling cards  
+        {
+            int r = i + rand.nextInt(cards.size() - i);  //getting value of r in the range from 0 to cards.size()-i
+              
+             //swapping the elements
+             Card temp = cards.get(r);
+             cards.set(r, cards.get(i));
+             cards.set(i, temp);  
+        }
+	}
+	
 	public ArrayList<Card> randomShuffledCards(int n, String type){
 		if(type == "SPADE") {              
-			ArrayList<Card> cards = getSpadeCards();
-			// Shuffle Logic
-			Random rand = new Random();     
-	        for (int i = 0; i < 10; i++)     //shuffling 10 spade cards  
-	        {
-	            int r = i + rand.nextInt(10 - i);  //getting value of r in the range from 0 to 10-i
-	              
-	             //swapping the elements
-	             Card temp = cards.get(r);
-	             cards.set(r, cards.get(i));
-	             cards.set(i, temp);  
-	        }
-	        
-	        ArrayList<Card> firstFourCards =  new ArrayList<Card>(cards.subList(0, n));   //n represents first n cards selected from 10 shuffled spade cards
+			shuffleLogic(spadeCards);
+				        
+	        ArrayList<Card> firstFourCards =  new ArrayList<Card>(spadeCards.subList(0, n));   //n represents first n cards selected from 10 shuffled spade cards
 
-	        cards.removeAll(firstFourCards);   // Delete first four cards from spadeCards
+	        spadeCards.removeAll(firstFourCards);   // Delete first four cards from spadeCards
 	        
 			return firstFourCards;
 		}else if(type == "FACECARD"){
-			ArrayList<Card> cards = getFaceCards();
-			
-			// Shuffle Logic
-			Random rand = new Random();     
-	        for (int i = 0; i < 12; i++)     //shuffling 12 face cards 
-	        {
-	            int r = i + rand.nextInt(12 - i);   //getting value of r in the range from 0 to 12-i
-	              
-	             //swapping the elements
-	             Card temp = cards.get(r);
-	             cards.set(r, cards.get(i));
-	             cards.set(i, temp);
-	               
-	        }
+			shuffleLogic(faceCards);
 	        
-	        ArrayList<Card> firstFourCards =  new ArrayList<Card>(cards.subList(0, n));  //n represents first n cards selected from 12 shuffled face cards
+	        ArrayList<Card> firstFourCards =  new ArrayList<Card>(faceCards.subList(0, n));  //n represents first n cards selected from 12 shuffled face cards
 
-	        cards.removeAll(firstFourCards);      // Delete first four cards from faceCards
+	        faceCards.removeAll(firstFourCards);      // Delete first four cards from faceCards
 	        
 			return firstFourCards;
-			
 		}else{ 
 	        ArrayList<Card> cards = concatenate(spadeCards, faceCards, normalCards);  //remaining cards
-	       
-	        // Shuffle Logic
-	        Random rand = new Random();     
-	        for (int i = 0; i < 44; i++)    //shuffling remaining cards 
-	        {
-	            int r = i + rand.nextInt(44 - i);   //getting value of r in the range from 0 to 44-i
-	              
-	             //swapping the elements
-	             Card temp = cards.get(r);
-	             cards.set(r, cards.get(i));
-	             cards.set(i, temp);
-	        }
+	        shuffleLogic(cards);
 	        
 	        return cards;
 		}
 	}
-	
 	
 }
